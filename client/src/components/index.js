1e7f3f79e98f831df3f7d4cf134e245ab943eff2
Omar Sakka amini <omarsakkaamini@gmail.com>
2023-11-01 13:48:20 +0400

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,12 @@
+import CustomButton from "./CustomButton";
+import AIPicker from "./AIPicker";
+import ColorPicker from "./ColorPicker.jsx";
+import FilePicker from "./FilePicker";
+import Tap from "./Tap";
+export{
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker,
+    Tap,
+};
